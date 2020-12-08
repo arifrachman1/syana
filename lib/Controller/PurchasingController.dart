d23ed54d5db89e80b24ac219ea697f06ef318162
yoga <yoraag@gmail.com>
2020-12-08 10:13:29 +0700

    PurchasingController - setApprovalPurchasing

--- lib/Controller/PurchasingController.dart
+++ lib/Controller/PurchasingController.dart
@@ -4,6 +4,8 @@ import 'package:syana/models/PurchasingModel.dart';
 import 'package:syana/models/UserModel.dart';
 import 'package:syana/utils/GlobalFunctions.dart';
 import 'package:syana/utils/GlobalVars.dart';
+import 'package:syana/utils/Strings.dart';
+import 'package:syana/widgets/CustomDialog.dart';
 
 class PurchasingController {
   BuildContext _context;
@@ -25,13 +27,8 @@ class PurchasingController {
     loadingStateCallback();
     FormData formData;
 
-    Map param = GlobalFunctions.generateMapParam([
-      "id_employee",
-      "list_type"
-    ], [
-      _userModel.id.toString(),
-      type
-    ]);
+    Map param = GlobalFunctions.generateMapParam(
+        ["id_employee", "list_type"], [_userModel.id.toString(), type]);
     formData = FormData.fromMap(param);
     print(formData.fields);
 
@@ -63,5 +60,37 @@ class PurchasingController {
       }
     } else {}
     loadingStateCallback();
+  }
+
+  setApprovalPurchasing(context, loadingStateCallback, setDataCallback,
+      idPurchasingSub, statusApproval) async {
+    if (_userModel == null) {
+      await _getPersistence();
+    }
+
+    var params = GlobalFunctions.generateMapParam(
+        ['id_purchasing_submission', 'status_approval'],
+        [idPurchasingSub, statusApproval]);
+
+    FormData formData = FormData.fromMap(params);
+
+    final data = await GlobalFunctions.dioPostCall(
+      context: context,
+      params: formData,
+      path: GlobalVars.baseUrl + "syana/purchasing/approval-purchasing",
+      options: Options(
+          headers: {"Authorization": "Bearer " + _userModel.accessToken}),
+    );
+
+    if (data != null) {
+      if (data['status'] == 200) {
+        Navigator.pop(context);
+        CustomDialog.getDialog(
+            title: Strings.DIALOG_TITLE_SUCCESS,
+            message: Strings.DIALOG_MESSAGE_CUSTOMER_SAVED,
+            context: context,
+            popCount: 1);
+      }
+    }
   }
 }
