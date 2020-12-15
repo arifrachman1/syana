6c42d155bae129eb188ec636491ccfb3d49184d4
hasbi908 <lbikang908@gmail.com>
2020-12-15 12:08:45 +0700

    History Detail dan Advertising Detail UI

---
+++ lib/screens/sdm/SyanaMenuMonitoring.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:syana/utils/AppTheme.dart';
+
+class SyanaMenuMonitoring extends StatefulWidget {
+  @override
+  _SyanaMenuMonitoringState createState() => _SyanaMenuMonitoringState();
+}
+
+class _SyanaMenuMonitoringState extends State<SyanaMenuMonitoring> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
