d6313e41fe8ca53c6693ebb10c6688da03f4abbe
bahtiar404 <kang.tekaje@gmail.com>
2020-12-17 14:25:57 +0700

    UI produksi

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme:
           ThemeData(primarySwatch: Colors.green, highlightColor: Colors.green),
-      home: SyanaMenuSDM(),
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
