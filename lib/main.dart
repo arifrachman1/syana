1ea41ead4a6a49a9ea33727a376f1a0973176d21
hasbi908 <lbikang908@gmail.com>
2020-12-18 18:03:39 +0700

    UI Menu Produksi Owner dan Menu History Produksi Owner

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:syana/screens/credentials/SplashScreen.dart';
-<<<<<<< HEAD
-=======
-import 'package:syana/screens/sdm/SyanaMenuSDM.dart';
->>>>>>> 4ad62525f24de6da6fa193d4b92482ac88b8f08a
 
 void main() => runApp(MyApp());
 
